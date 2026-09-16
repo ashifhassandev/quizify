b4bc410a9a78939b74ef192c878c2c7d9f8c9b71
Ashif Hassan <ashifhassan1996@gmail.com>
2026-09-16 21:37:47 +0530

    style: format code with Prettier

--- models/quizHistoryModel.ts
+++ models/quizHistoryModel.ts
@@ -2,36 +2,36 @@ import mongoose, { Document, Model, Schema, ObjectId } from "mongoose";
 
 // Define the interface for the QuizHistory document
 export interface QuizHistoryDocument extends Document {
-    _id: ObjectId;
-    userId: mongoose.Types.ObjectId;
-    quizId: string;
-    score: number;
-    completedAt: Date;
+  userId: mongoose.Types.ObjectId;
+  quizId: string;
+  score: number;
+  completedAt: Date;
 }
 
 const quizHistorySchema = new Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-    quizId: {
-        type: String,
-        required: true,
-    },
-    score: {
-        type: Number,
-        required: true,
-    },
-    completedAt: {
-        type: Date,
-        default: Date.now,
-    },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  quizId: {
+    type: String,
+    required: true,
+  },
+  score: {
+    type: Number,
+    required: true,
+  },
+  completedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 quizHistorySchema.index({ userId: 1 });
 quizHistorySchema.index({ completedAt: -1 });
 
-const QuizHistory: Model<QuizHistoryDocument> = mongoose.model<QuizHistoryDocument>("QuizHistory", quizHistorySchema);
+const QuizHistory: Model<QuizHistoryDocument> =
+  mongoose.model<QuizHistoryDocument>("QuizHistory", quizHistorySchema);
 
 export default QuizHistory;
--- models/userModel.ts
+++ models/userModel.ts
@@ -2,91 +2,91 @@ import mongoose, { Document, Model, Schema, ObjectId } from "mongoose";
 import bcrypt from "bcrypt";
 
 export interface UserDocument extends Document {
-    _id: ObjectId;
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    role: string;
-    isVerified: boolean;
-    verificationToken?: string;
-    verificationTokenExpires?: Date;
-    quizHistory: mongoose.Types.ObjectId[];
-    totalPoints: number;
-    createdAt: Date;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  role: string;
+  isVerified: boolean;
+  verificationToken?: string;
+  verificationTokenExpires?: Date;
+  quizHistory: mongoose.Types.ObjectId[];
+  totalPoints: number;
+  createdAt: Date;
 }
 
 const userSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true,
-        trim: true,
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: {
+      validator: (val) => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val);
+      },
+      message: "Invalid email format",
     },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+  },
+  role: {
+    type: String,
+    required: true,
+    default: "User",
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+  },
+  verificationTokenExpires: {
+    type: Date,
+  },
+  quizHistory: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "QuizHistory",
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        validate: {
-            validator: (val) => {
-                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val);
-            },
-            message: "Invalid email format",
-        },
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 8,
-    },
-    role: {
-        type: String,
-        required: true,
-        default: "User",
-    },
-    isVerified: {
-        type: Boolean,
-        default: false,
-    },
-    verificationToken: {
-        type: String,
-    },
-    verificationTokenExpires: {
-        type: Date,
-    },
-    quizHistory: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "QuizHistory",
-        },
-    ],
-    totalPoints: {
-        type: Number,
-        default: 0,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  ],
+  totalPoints: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-userSchema.index({ email: 1 }, { unique: true });
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const salts = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salts);
+  }
 
-userSchema.pre("save", async function(next) {
-    if (this.isModified("password")) {
-        const salts = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salts);
-    }
-
-    next();
+  next();
 });
 
-const User: Model<UserDocument> = mongoose.model<UserDocument>("User", userSchema);
+const User: Model<UserDocument> = mongoose.model<UserDocument>(
+  "User",
+  userSchema,
+);
 
 export default User;
