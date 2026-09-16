b3e8e2267e52632368281070a0201bdfd78d723f
Ashif Hassan <ashifhassan1996@gmail.com>
2026-09-16 20:26:55 +0530

    style: format code with Prettier

--- config/dbConfig.ts
+++ config/dbConfig.ts
@@ -5,15 +5,15 @@ import mongoose from "mongoose";
 
 // Establish MongoDB connection
 const connectToDatabase = async () => {
-    try {
-        const connection = await mongoose.connect(process.env.MONGO_URI);
-        const connectionDetails = `${connection.connection.host}:${connection.connection.port}/${connection.connection.name}`;
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+    const connectionDetails = `${connection.connection.host}:${connection.connection.port}/${connection.connection.name}`;
 
-        console.log(`MongoDB connected: ${connectionDetails}`);
-    } catch (error) {
-        console.error(`Database connection error: ${(error as Error).message}`);
-        process.exit(1);
-    }
+    console.log(`MongoDB connected: ${connectionDetails}`);
+  } catch (error) {
+    console.error(`Database connection error: ${(error as Error).message}`);
+    process.exit(1);
+  }
 };
 
 export default connectToDatabase;
--- config/emailConfig.ts
+++ config/emailConfig.ts
@@ -5,11 +5,11 @@ import nodemailer from "nodemailer";
 
 // Create a transporter object using Gmail SMTP service
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.SEND_OTP_EMAIL,
-        pass: process.env.SEND_OTP_EMAIL_PASS,
-    },
+  service: "gmail",
+  auth: {
+    user: process.env.SEND_OTP_EMAIL,
+    pass: process.env.SEND_OTP_EMAIL_PASS,
+  },
 });
 
 export default transporter;
